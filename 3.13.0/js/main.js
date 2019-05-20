e2c1be6b6b1953fb7b2b4400eb3cf02eaa5f989b
Clark Tyra <clarktyra@Clarks-MacBook-Pro.local>
2019-05-20 14:21:11 -0700

    converted the data from strings to numbers

--- 3.13.0/js/main.js
+++ 3.13.0/js/main.js
@@ -4,3 +4,11 @@
 *    Project 1 - Star Break Coffee
 */
 
+var data = d3.json("data/revenues.json").then(function(data){
+    data.forEach(function(d){
+        d.revenue = +d.revenue;
+        d.profit = +d.profit;
+    })
+    
+    console.log(data)
+})
