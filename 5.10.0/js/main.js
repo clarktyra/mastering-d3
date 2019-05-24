79430b635fb3798583e2c9c6645301ccc561ca21
Clark Tyra <clark.tyra@gmail.com>
2019-05-24 10:11:41 -0700

    filtered the data to get rid of nulls for incomes and life_exp

--- 5.10.0/js/main.js
+++ 5.10.0/js/main.js
@@ -4,6 +4,10 @@
 *    Project 2 - Gapminder Clone
 */
 
-d3.json("data/data.json").then(function(data){
-	console.log(data);
+d3.json("data/data.json").then(function (data) {
+	data.forEach(x =>
+		x.countries = x.countries.filter(thing => (thing.income !== null) && (thing.life_exp !== null))
+	);
+	console.log(data)
+
 })
